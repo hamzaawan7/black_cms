0fb6d89521c197cec10c341d8f9c3cec6dcc8835
“Hamza <saladinian@gmail.com>
2026-01-22 16:40:25 +0500

    Fix: Update .htaccess routing and root index.php

--- index.php
+++ index.php
@@ -5,8 +5,18 @@
  * This file allows running Laravel from the root of public_html on shared hosting.
  */
 
-// Change directory to public folder and run the app
-chdir(__DIR__ . '/public');
+use Illuminate\Http\Request;
 
-// Include the main index.php from public folder
-require __DIR__ . '/public/index.php';
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/bootstrap/app.php')
+    ->handleRequest(Request::capture());
