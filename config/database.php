4aa7d616ba56626714e7788a7b16bf620e5ede4a
“Hamza <saladinian@gmail.com>
2026-01-22 15:47:01 +0500

    Fix: Add .env file and include vendor folder in deployment

--- config/database.php
+++ config/database.php
@@ -62,6 +62,25 @@ return [
             ]) : [],
         ],
 
+        'mysql_production' => [
+            'driver' => 'mysql',
+            'host' => env('DB_PROD_HOST', '156.67.67.67'),
+            'port' => env('DB_PROD_PORT', '3306'),
+            'database' => env('DB_PROD_DATABASE', 'u938549775_hyvecms'),
+            'username' => env('DB_PROD_USERNAME', 'u938549775_hyvecms'),
+            'password' => env('DB_PROD_PASSWORD', ''),
+            'unix_socket' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
